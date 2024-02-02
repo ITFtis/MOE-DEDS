c16a03ffb8c3cec43f4d97c4f2bc2e0c6ca3bf79
brianlin12345 <brianlin12345@gamil.com>
2024-02-02 16:11:05 +0800

    ignore v1

--- DEDS/obj/Release/Package/PackageTmp/Scripts/prj/dfa.js
+++
@@ -1,8 +0,0 @@
-﻿/*人工圈劃*/
-$(document).ready(function () {
-    mapHelper.createMap('leaflet', function () {
-        setTimeout(function () {
-            $("#main-ctrl").handDrawFlood({ map: app.map }).trigger($.menuctrl.eventKeys.popu_init_before);
-        }, 1000);
-    });
-})
--- DEDS/obj/Release/Package/PackageTmp/Scripts/prj/ef.js
+++
@@ -1,8 +0,0 @@
-﻿/* 人工匯入災情controller */
-$(document).ready(function () {
-    mapHelper.createMap('leaflet', function () {
-        setTimeout(function () {
-            $("#main-ctrl").estimateFlood({ map: app.map }).trigger($.menuctrl.eventKeys.popu_init_before);
-        }, 1000);
-    });
-})
