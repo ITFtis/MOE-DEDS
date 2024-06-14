250d17e97f78c526b529d751ead68007073fbfe2
brianlin12345 <brianlin12345@gamil.com>
2024-06-14 10:52:13 +0800

    script

--- DEDS/UpdateScript/20240614_查詢組織環境部環保署條件.sql
+++ DEDS/UpdateScript/20240614_查詢組織環境部環保署條件.sql
@@ -1,9 +1,9 @@
---1.新增欄位(ConUnitCode)
-alter TABLE ConUnitCode Add UserOrg1 int Null
+--1.新增欄位(ConUnitCode)　　大單位組織1　
+alter TABLE ConUnitCode Add BigOrg1 int Null
 Go
 
 --2.補上資料(ConUnitCode)
-Update ConUnitCode Set UserOrg1 = 1 Where Code Like 'a%'
-Update ConUnitCode Set UserOrg1 = 2 Where Code Like 'b%'
+Update ConUnitCode Set BigOrg1 = 1 Where Code Like 'a%'
+Update ConUnitCode Set BigOrg1 = 2 Where Code Like 'b%'
 Go
 
