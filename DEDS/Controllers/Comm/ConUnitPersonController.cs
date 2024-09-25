923c221fb38acc1368cd57e3bb43796f09b1a930
brianlin12345 <brianlin12345@gamil.com>
2024-09-25 15:25:14 +0800

    pdf 超過A4

--- DEDS/Controllers/Comm/ConUnitPersonController.cs
+++ DEDS/Controllers/Comm/ConUnitPersonController.cs
@@ -362,7 +362,7 @@ namespace DEDS.Controllers.Comm
                 //產出excel
                 List<string> titles = new List<string>();
                 //"0":不調整width,"1":自動調整長度(效能差:資料量多),"2":字串長度調整width,"3":字串長度調整width(展開)
-                int autoSizeColumn = 2;
+                int autoSizeColumn = 3;
 
                 string fileName = DEDS.ExcelSpecHelper.GenerateExcelByLinqF2(fileTitle, titles, list, folder, autoSizeColumn, true, topContents);
                 string path = folder + fileName;                //.xlsx
@@ -540,7 +540,7 @@ namespace DEDS.Controllers.Comm
                 //產出excel
                 List<string> titles = new List<string>();
                 //"0":不調整width,"1":自動調整長度(效能差:資料量多),"2":字串長度調整width,"3":字串長度調整width(展開)
-                int autoSizeColumn = 2;
+                int autoSizeColumn = 3;
 
                 string fileName = DEDS.ExcelSpecHelper.GenerateExcelByLinqF2(fileTitle, titles, list, folder, autoSizeColumn, true, topContents);
                 string path = folder + fileName;                //.xlsx
